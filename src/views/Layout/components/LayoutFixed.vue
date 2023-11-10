c0816b7777a4b13d656e42c8cd171d54f074409e
fysxaki <baetsu314@gmail.com>
2023-11-10 21:06:13 +0800

    pinia优化导航数据

--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { useCategoryStore } from '@/stores/category';
 // vueUse
 import { useScroll } from '@vueuse/core'
-
-// const el = ref<HTMLElement | null>(null)
 const { y } = useScroll(window)
+
+// use the data from pinia
+const categoryStore = useCategoryStore()
 </script>
 
 <template>
@@ -12,35 +14,8 @@ const { y } = useScroll(window)
       <RouterLink class="logo" to="/" />
       <!-- 导航区域 -->
       <ul class="app-header-nav ">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">居家</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">美食</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">服饰</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">母婴</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">个护</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">严选</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">数码</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">运动</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">杂项</RouterLink>
+        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
+          <RouterLink to="/">{{item.name}}</RouterLink>
         </li>
       </ul>
 
