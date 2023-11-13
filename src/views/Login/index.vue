c2406a57a07ec4debd18d820d4d785fb54ac68af
fysxaki <baetsu314@gmail.com>
2023-11-13 16:00:36 +0800

    登录-基础业务实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,11 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
+import {LoginAPI} from '@/apis/user'
+
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+
+import {useRouter} from 'vue-router'
 // 表单校验功能(账户名及密码)
 // 1. 准备表单对象
 const form = ref({
@@ -35,18 +41,30 @@ const rules = {
 
 //3 . 获取form实例做统一校验
 const formRef = ref(null)
-
+const router = useRouter()
 const doLogin = () => {
+  const {account, password} = form.value
+
   // 调用实例方法
-  formRef.value.validate((valid) => {
+  formRef.value.validate(async (valid) => {
     // valid: 所有表单都通过校验, 才为true
     console.log(valid);
     // 以valid作为判断条件 如果通过校验才执行登录逻辑
     if (valid) {
       // TODO LOGIN
+      const res = await LoginAPI({account, password})
+      console.log(res);
+      //1.提示用户
+      ElMessage({type:'success', message:'登录成功'})
+      //2.跳转首页
+      router.replace({path:'/'})
     }
   })
 }
+
+// 1.用户名和密码 只需要通过简单的配置(看文档的形式 - 复杂的功能通过不同的组件拆解)
+// 2.同意协议通过自定义规则实现 validator:(rule,value,callback) => {}这里callback必须要执行
+// 3. 统一校验  通过form实例的方法 validate -> true
 </script>
 
 
