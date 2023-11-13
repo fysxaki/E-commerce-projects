8e28bd1815e89d8c89e88d50fd36e03967c38a7a
fysxaki <baetsu314@gmail.com>
2023-11-13 12:29:15 +0800

    详情页-全局组件插件化

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+// components中的所以组件全局化注册
+// 通过插件的方式
+import ImageView from "./ImageView/index.vue";
+import Sku from "./XtxSku/index.vue";
+
+export const componentsPlugin = {
+  install(app) {
+    // app.component('组件名字',组件配置对象)
+    app.component("XtxImageView", ImageView);
+    app.component("XtxSku", Sku);
+  },
+};
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,10 +1,10 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
-import ImageView from '@/components/ImageView/index.vue'
+
 import { getDetail } from '@/apis/detail'
 import { useRoute } from 'vue-router'
 import {onMounted,ref} from 'vue'
-import XtxSku from '@/components/XtxSku/index.vue'
+
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -43,7 +43,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures"/>
+              <XtxImageView :image-list="goods.mainPictures"/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
