986bcfb13962c26ef9a9d9eb64cb052e9b50379b
fysxaki <baetsu314@gmail.com>
2023-11-14 23:03:12 +0800

    退出登录时清除购物车数据

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -3,10 +3,12 @@
 import { defineStore } from "pinia";
 import { LoginAPI } from "@/apis/user";
 import { ref } from "vue";
+import { useCartStore } from "./cartStore";
 
 export const useUserStore = defineStore(
   "user",
   () => {
+    const cartStore = useCartStore();
     // 1. 定义管理用户数据的state
     const userInfo = ref({});
     // 2. 定义获取接口数据的action函数
@@ -17,6 +19,8 @@ export const useUserStore = defineStore(
     // 退出时清除用户信息
     const clearUserInfo = () => {
       userInfo.value = {};
+      // 执行清除购物车的action
+      cartStore.clearCart();
     };
     // 3. 以对象的形式把state和action return
     return {
